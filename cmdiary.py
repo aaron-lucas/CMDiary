14080c69457c1d09113889d5cfa59186442f2f36
Aaron Lucas <aaron.lucas1@icloud.com>
2015-04-05 15:14:53 +1000

    Add some regexs for parsing input

--- cmdiary.py
+++ cmdiary.py
@@ -1,5 +1,46 @@
 from DiaryEntry import ASSESSMENT, HOMEWORK, NOTE, UID, ITEM_TYPE, SUBJECT, DESCRIPTION, DUE_DATE
 from Diary import Diary
+from termcolor import cprint, colored
+import re
+
+diary = Diary()
+
+def get_input(prompt, response_type=str, condition=None, modifier=None):
+	while True:
+		inp = input(prompt)
+		try:
+			if inp:
+				inp = response_type(inp)
+		except:
+			cprint('Input must be of type {}'.format(response_type.__name__), 'yellow')
+		else:
+			if condition is None or condition(inp):
+				return inp if modifier is None else modifier(inp)
+			cprint('Input must pass specified condition', 'yellow')
+
+def format_input(input_string):
+	pass
+
+def add(input_data):
+	re_command = re.compile('^([a-zA-Z]*?) ')
+	re_item_type = re.compile('([ahn]|assessment|homework|note)')
+	re_subject = re.compile(r' (.*?)[\;]')
+	re_description = re.compile('r[\;](.*?)[\;]')
+	re_due_date = re.compile(r'[\;](([0-9]{1,2}){1,2}([0-9]{4})?)$')
+
+
+def remove(input_data):
+	pass
+
+def edit(input_data):
+	pass
+
+def extend(input_data):
+	pass
+
+def list():
+	pass
+
 ABBREVIATIONS = {'a': (add, ASSESSMENT),
                 'd': DESCRIPTION,
                 'due': DUE_DATE,
@@ -14,26 +55,3 @@ ABBREVIATIONS = {'a': (add, ASSESSMENT),
                 't': ITEM_TYPE,
                 'u': UID,
                 'x': extend}
-
-diary = Diary()
-
-def get_input():
-	pass
-
-def format_input(input_string):
-	pass
-
-def add(item_type, subject, description, due_date):
-	pass
-
-def remove(*uids):
-	pass
-
-def edit(attr, value, *uids):
-	pass
-
-def extend(days, *uids):
-	diary.extend(days, *uids)
-
-def list():
-	pass
